9da941e13b33176e6d025ca15cc435e7d34bcfa3
Belino19 <104334710+Belino19@users.noreply.github.com>
2025-05-12 00:15:40 +0800

    Insurance

--- src/app/main/insurance/insurance.component.ts
+++ src/app/main/insurance/insurance.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-insurance',
@@ -8,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class InsuranceComponent {
 
+    displayedColumns: string[] = [
+  'certificateNo',
+  'fullName',
+  'age',
+  'status',
+  'effectiveDate',
+  'expiryDate',
+  'term',
+  'sumInsured',
+  'grossPremium'
+];
+
+dataSource = new MatTableDataSource([
+  {
+    certificateNo: '01234567',
+    fullName: 'Castillo, Andrea Louise X',
+    age: 34,
+    status: 'N',
+    effectiveDate: 'March 15, 2025',
+    expiryDate: 'March 15, 2027',
+    term: 12,
+    sumInsured: '60,000',
+    grossPremium: '2,000'
+  },
+  {
+    certificateNo: '01234567',
+    fullName: 'Abagansya, Teodore Peter I',
+    age: 23,
+    status: 'F',
+    effectiveDate: 'March 15, 2025',
+    expiryDate: 'March 15, 2027',
+    term: 12,
+    sumInsured: '60,000',
+    grossPremium: '683'
+  }
+]);
+
 }
+

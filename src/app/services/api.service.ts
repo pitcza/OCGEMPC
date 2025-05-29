cba9270e3d32bea55772e8cd346926402eba6ba8
pitcza <czarinaarellano12@gmail.com>
2025-05-29 09:16:11 +0800

    try

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
 
-  private baseUrl = 'http://localhost:4000/api'; 
+  private baseUrl = 'https://ocgempcapi-production.up.railway.app/api'; 
 
   constructor(private http: HttpClient) { }
 
